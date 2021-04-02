366dba980c20eb7edc2db340cd575f1d7491b2ce
simonstockton763@gmail.com <simon.stockton763@gmail.com>
2021-04-02 12:33:30 -0500

    Created functionality for Email Manager Created primitive customer class

---
+++ EasyPay/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Customer (string a, string b)
+        {
+            Name = a;
+            Email = b;
+        } 
+
+    }
+}
---
+++ EasyPay/EmailManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentEmail.Core;
+using FluentEmail.Smtp;
+using System.Net.Mail;
+using System.Net;
+
+namespace EasyPay
+{
+    class EmailManager
+    {
+        public Customer recepient;
+       
+        SmtpSender sender = new SmtpSender(() => new SmtpClient(host: "Smtp.gmail.com")
+        {
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential("Username","Password"),
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+        });
+        
+               
+
+        public void SendEmail()
+        {
+            sender.Send(Email
+                .From(emailAddress: "noReply@noReply.com")
+                .To(emailAddress: recepient.Email, name: recepient.Name)
+                .Subject(subject: "Service Invoice")
+                .Body(body: "This is an email to inform you of a requested payment from Company A")
+               );
+        }
+    }
+}
