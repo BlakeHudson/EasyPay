0549f6f501233781be5ebed52ecd9a47228c5994
Lily Thompson <lilywashereagain@gmail.com>
2021-04-06 17:36:43 -0500

    Stuff for connecting user to DB

---
+++ EasyPay/SQLiteDataAccess.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay
+{
+    public class SQLiteDataAccess
+    {
+        public List<EasyPayUser> LoadUsers()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<EasyPayUser>("select * from User", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void SaveUser(EasyPayUser user)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into User (UserName, Password) values (@USER_NAME, @PASSWORD)", user);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
