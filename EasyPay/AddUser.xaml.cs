b32b832711e45edec160fddc17b49bdf9caa4669
BlakeHudson <blakehudson615@gmail.com>
2021-04-16 09:36:28 -0500

    Added confirm password feature for adding an EasyPayUser

--- EasyPay/AddUser.xaml.cs
+++ EasyPay/AddUser.xaml.cs
@@ -25,26 +25,42 @@ namespace EasyPay
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Upon addUser_Submit_Click the String in the TextBox and Password in the Password box will be applied sent to
+        /// the constructor for an EasyPayUser to be created called newUser.
+        /// newUSer will then be passed to the SQLLiteDataAccess SaveUser function, saving it to the database.
+        /// A messagebox will display informing the user of a sucessful save.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        
         private void addUser_Submit_Click(object sender, RoutedEventArgs e)
         {
+            
             String un;
             String pw;
+            String confirmPw;
             EasyPayUser newUser;
 
             un = newUsername.Text;
             pw = newPasswordBox.Password;
+            confirmPw = confirmPasswordBox.Password;
 
-            newUser = new EasyPayUser(un, pw);
-            SQLiteDataAccess.SaveUser(newUser);
+            if(pw == confirmPw)
+            {
+                newUser = new EasyPayUser(un, pw);
+                SQLiteDataAccess.SaveUser(newUser);
 
-            MessageBox.Show("New user added");
+                MessageBox.Show("New user added");
 
-            MainWindow dashboard = new MainWindow();
-            dashboard.Show();
-            this.Close();
-        }
-    }
+                MainWindow dashboard = new MainWindow();
+                dashboard.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please make sure passwords match");
+            }
 
 
 }
